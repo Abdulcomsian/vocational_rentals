0a00cfd2774dce57d19b9ade2c33a271d795d5f4
urraish <urraish.raza@accrualhub.com>
2024-02-28 15:21:03 +0500

    design changes

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -11,51 +11,56 @@ function Signin() {
         <header className="mt-3">
           <Image src={Logo} alt="" height={50} />
         </header>
-        <section className="signin-module">
-          <Link href="/" className="back">
-            <i className="las la-long-arrow-alt-left"></i>
-            <span>Back</span>
-          </Link>
-          <h3 className="title">Sign In</h3>
-          <form>
-            <div className="mb-4">
-              <label for="exampleInputEmail1" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="You email"
-              />
-            </div>
-            <div className="mb-4">
-              <label for="exampleInputPassword1" className="form-label">
-                Password
-              </label>
-              <div className="pass-view">
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-4 col-xl-4 ">
+            <section className="signin-module">
+            <Link href="/" className="back">
+              <i className="las la-long-arrow-alt-left"></i>
+              <span>Back</span>
+            </Link>
+            <h3 className="title">Sign In</h3>
+            <form>
+              <div className="mb-4">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email
+                </label>
                 <input
-                  type="password"
+                  type="email"
                   className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Create password"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="You email"
                 />
-                <a href="javascript:void(0)" className="view-pass">
-                  <i className="las la-eye"></i>
-                </a>
               </div>
-            </div>
-            <div className="mb-4 form-check text-end">
-              <label className="form-check-label" for="exampleCheck1">
-                <Link href="#">Forgot Password?</Link>
-              </label>
-            </div>
-            <button type="submit" className="btn submit mt-5">
-              Sign In
-            </button>
-          </form>
-        </section>
+              <div className="mb-4">
+                <label for="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <div className="pass-view">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Create password"
+                  />
+                  <a href="javascript:void(0)" className="view-pass">
+                    <i className="las la-eye"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="mb-4 form-check text-end">
+                <label className="form-check-label" for="exampleCheck1">
+                  <Link href="#">Forgot Password?</Link>
+                </label>
+              </div>
+              <button type="submit" className="btn submit mt-5">
+                Sign In
+              </button>
+            </form>
+          </section>
+          </div>
+        </div>
+        
       </div>
     </>
   );
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -12,70 +12,75 @@ function Signin() {
             <Image src={Logo} alt="" height={50} />
           </Link>
         </header>
-        <section className="signup-module">
-          <Link href="/" className="back">
-            <i className="las la-long-arrow-alt-left"></i>
-            <span>Back</span>
-          </Link>
-          <h3 className="title">Sign Up</h3>
-          <form>
-            <div className="mb-4">
-              <label for="exampleInputEmail1" className="form-label">
-                Full Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="You name"
-              />
-            </div>
-            <div className="mb-4">
-              <label for="exampleInputEmail1" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="You email"
-              />
-            </div>
-            <div className="mb-4">
-              <label for="exampleInputPassword1" className="form-label">
-                Password
-              </label>
-              <div className="pass-view">
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-lg-4 col-xl-4 ">
+            <section className="signup-module">
+            <Link href="/" className="back">
+              <i className="las la-long-arrow-alt-left"></i>
+              <span>Back</span>
+            </Link>
+            <h3 className="title">Sign Up</h3>
+            <form>
+              <div className="mb-4">
+                <label for="exampleInputEmail1" className="form-label">
+                  Full Name
+                </label>
                 <input
-                  type="password"
+                  type="text"
                   className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Create password"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="You name"
                 />
-                <a href="javascript:void(0)" className="view-pass">
-                  <i className="las la-eye"></i>
-                </a>
               </div>
-            </div>
-            <div className="mb-4 form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id="exampleCheck1"
-              />
-              <label className="form-check-label" for="exampleCheck1">
-                Please confirm that you have read and agree to our{" "}
-                <Link href="/term-conditions">terms & conditions</Link> and{" "}
-                <Link href="/privacy-policy">privacy policy</Link>
-              </label>
-            </div>
-            <button type="submit" className="btn submit mt-5">
-              Sign Up
-            </button>
-          </form>
-        </section>
+              <div className="mb-4">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="You email"
+                />
+              </div>
+              <div className="mb-4">
+                <label for="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <div className="pass-view">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Create password"
+                  />
+                  <a href="javascript:void(0)" className="view-pass">
+                    <i className="las la-eye"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="mb-4 form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" for="exampleCheck1">
+                  Please confirm that you have read and agree to our{" "}
+                  <Link href="/term-conditions">terms & conditions</Link> and{" "}
+                  <Link href="/privacy-policy">privacy policy</Link>
+                </label>
+              </div>
+              <button type="submit" className="btn submit mt-5">
+                Sign Up
+              </button>
+            </form>
+          </section>
+          </div>
+        </div>
+        
       </div>
     </>
   );
